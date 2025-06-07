6d73a9ee43c3a375469500150a2123fd17bac4cb
Andrew <andrew.shedov@gmail.com>
2025-06-07 13:27:33 +0300

    commit

--- npm/src/mini-maker.js
+++ npm/src/mini-maker.js
@@ -19,7 +19,7 @@ const getByPath = (obj, path) => {
   return path.split('.').reduce((acc, part) => acc?.[part], obj);
 };
 
-// One sentence generator
+// one sentence generator
 const sentence = ({ wordMin = 4, wordMax = 10, hashtagMin = 0, hashtagMax = 2 }) => {
   const wordsCount = int({ min: wordMin, max: wordMax });
   const sentenceWords = Array.from({ length: wordsCount }, () => word(storage.words));
